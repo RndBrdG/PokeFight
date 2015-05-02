37e3ed2c1e8aa1cb4074ee27b76912b1f399e787
Cowtard <ei12109@fe.up.pt>
2015-05-02 16:28:46 +0100

    Primeiro commit

---
+++ desktop/src/com/pokefight/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pokefight.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pokefight.game.PokeFight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PokeFight(), config);
+	}
+}
