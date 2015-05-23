ffa206bddf9fb66ab69d48a6acb648b11830e8e3
Joao Costa <sd_joao@hotmail.com>
2015-05-23 16:20:47 +0100

    barra+lifebox

--- core/src/com/pokefight/gamescreens/BattleScreen.java
+++ core/src/com/pokefight/gamescreens/BattleScreen.java
@@ -11,16 +11,32 @@ import com.pokejava.Sprite;
 
 public class BattleScreen implements Screen{
 	SpriteBatch batch = new SpriteBatch();
-	Sprite charmander = new Sprite(5);
-	Sprite charizard = new Sprite(2);
-	Texture charmPoke = new Texture("." + charmander.getImage());
-	TextureRegion charmPoke1 = new TextureRegion(charmPoke);
-	Texture chariPoke = new Texture("." + charizard.getImage());
+	Sprite pokeenemy = new Sprite(6); // seleção do pokemon 
+	Sprite pokeme = new Sprite(2);
+	Texture pokeenemytext = new Texture("." + pokeenemy.getImage());
+	TextureRegion pokeenemytextreg = new TextureRegion(pokeenemytext);
+	Texture pokemetext = new Texture("." + pokeme.getImage());
+	TextureRegion pokemetextreg = new TextureRegion(pokemetext);
+	
+	Texture hpenemy = new Texture("media/img/hp-foe.png");
+	TextureRegion hpenemytextreg = new TextureRegion(hpenemy);
+	
+	Texture hpme = new Texture("media/img/hp-me.png");
+	TextureRegion hpmetextreg = new TextureRegion(hpme);
+	
+	Texture lifered = new Texture("media/img/hp-low.png");
+	TextureRegion lifered1 = new TextureRegion(lifered);
+	
+	Texture dialogbox = new Texture("media/img/dialog-box.png");
+	TextureRegion dialogboxtextreg = new TextureRegion(dialogbox);
+	
 
-	TextureRegion chariPoke1 = new TextureRegion(chariPoke);
+	
 	Texture battlefield = new Texture("battlefields/1.png");
 	private float count =360.0f;
 	private float count1 =100.0f;
+	private float count2 =0.0f;
+	private float count3 =48.0f;
 
 	public BattleScreen(){
 
@@ -30,7 +46,7 @@ public class BattleScreen implements Screen{
 	public void show() {
 	}
 	
-
+	
 	
 	@Override
 	public void render(float delta) {
@@ -38,9 +54,24 @@ public class BattleScreen implements Screen{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(battlefield, 0, 44, Gdx.app.getGraphics().getWidth(), Gdx.app.getGraphics().getHeight());
-		batch.draw(charmPoke1, (float)135, (float)70,(float) 40,(float) 40,(float) count1, (float)count1, (float)1, (float)1,(float)count);
-		batch.draw(chariPoke1, 6.f, 20.f,(float) 50,(float) 50,(float) 100, (float)100, (float)-1, (float)1,(float)0);
+		batch.draw(pokeenemytextreg, (float)200, (float)70,(float) 245,(float) 150,(float) count1, (float)count1, (float)1, (float)1,(float)0);
+		batch.draw(pokemetextreg, 6.f, 20.f,(float) 50,(float) 50,(float) 100, (float)100, (float)-1, (float)1,(float)0);
+		batch.draw(hpenemytextreg, 0.f, 180.f,(float) 50,(float) 50,(float) 122, (float)33, (float)1, (float)1,(float)0);
+		batch.draw(hpmetextreg, 272.f, 50.f,(float) 50,(float) 50,(float) 128, (float)42, (float)1, (float)1,(float)0);
+		batch.draw(lifered1, 50.f, 188.f,(float) 50,(float) 50,(float) count2, (float)2, (float)1, (float)1,(float)0);
+		batch.draw(lifered1, 344.f, 66.f,(float) 50,(float) 50,(float) count2, (float)2, (float)1, (float)1,(float)0);
+		batch.draw(dialogboxtextreg, 0.f, 0.f,(float) 50,(float) 50,(float) 400, (float)44, (float)1, (float)1,(float)0);
 		batch.end();
+		
+		if(count3 < 0.0f)
+			count3 = 48.0f;
+			else
+			count3 --;
+		
+		if(count2 > 47.0f)
+			count2 = 0.0f;
+			else
+			count2 ++;
 		
 		if(count1 < 0.0f)
 			count1 = 100.0f;
