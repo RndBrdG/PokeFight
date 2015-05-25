c779743cdf915cc24f551b861775f66eea6c59de
Joao Costa <sd_joao@hotmail.com>
2015-05-25 20:33:20 +0100

    Attack menu + hp level (with colors and percentage)
    With diferent functions

--- core/src/com/pokefight/gamescreens/BattleScreen.java
+++ core/src/com/pokefight/gamescreens/BattleScreen.java
@@ -2,12 +2,21 @@ package com.pokefight.gamescreens;
 
 import java.util.Random;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.pokefight.gameplay.Trainer;
 import com.pokejava.Sprite;
 
@@ -48,7 +57,18 @@ public class BattleScreen implements Screen{
 	private final TextureRegion dialogboxtextreg;
 	
 	private final Texture battlefield;
-
+	//MENUATTACK
+	 TextButtonStyle textButtonStyleFight, textButtonStylePokemon, textButtonStyleQuit;
+	 Skin skins;
+	 TextureAtlas buttonAtlas;
+	 private Skin skin;
+	 Stage stages;
+	 BitmapFont font;
+	 private Stage stage;
+	 private Texture img;
+	 //FLOAT TEST LIFE
+	 float HP1 = 20, HP2=100, MAXHP=200;
+	 
 	public BattleScreen(){
 		Random gerador = new Random();
 		int mapa = gerador.nextInt(11) + 1;
@@ -73,16 +93,18 @@ public class BattleScreen implements Screen{
 		my_pokemonStatus = new Texture("media/img/hp-me.png");
 		my_tr_pokemonStatus = new TextureRegion(my_pokemonStatus);
 		
-		enemy_currentHP = new Texture("media/img/hp-low.png");
+		enemy_currentHP = new Texture("media/img/"+ lifeBar(HP1, MAXHP));
 		enemy_tr_currentHP = new TextureRegion(enemy_currentHP);
 		
-		my_currentHP = new Texture("media/img/hp-low.png");
+		my_currentHP = new Texture("media/img/"+ lifeBar(HP2, MAXHP));
 		my_tr_currentHP = new TextureRegion(my_currentHP);
 		
 		dialogbox = new Texture("media/img/dialog-box.png");
 		dialogboxtextreg = new TextureRegion(dialogbox);
 		
 		battlefield = new Texture("battlefields/" + mapa + ".png");
+		
+		menuattack();
 		
 	}
 	
@@ -105,10 +127,15 @@ public class BattleScreen implements Screen{
 		batch.draw(enemy_tr_pokemonStatus, 0.f, 180.f, 50, 50, 122, 33, 1, 1, 0);
 		batch.draw(my_tr_pokemonStatus, 272.f, 50.f, 50, 50, 128, 42, 1, 1,0);
 		
-		batch.draw(enemy_tr_currentHP, 50.f, 188.f, 50, 50, 0, 2, 1, 1,0);
-		batch.draw(my_tr_currentHP, 344.f, 66.f, 50, 50, 0, 2, 1, 1,0);
-		
+		batch.draw(enemy_tr_currentHP, 50.f, 188.f, 50, 50, (HP1/MAXHP)*48, 2, 1, 1,0);
+		batch.draw(my_tr_currentHP, 344.f, 66.f, 50, 50, (HP2/MAXHP)*48, 2, 1, 1,0);
+		//menuattackbackground
+		batch.draw(img, 282, 0, 118, 46);
 		batch.end();
+		
+		//Menuattack
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
 		
 		/*
 		if(attack == 0){
@@ -145,10 +172,17 @@ public class BattleScreen implements Screen{
 		if(count < 0.0f)
 			count = 360.0f;
 			else
-			count --;
-	*/
+			count --;*/
+	
 	}
-
+	//CALC COLOR OF LIFE BAR (CRITIC, low, GOOD)
+	public String lifeBar(float hp, float maxhp){
+		float average = hp/maxhp;
+		if(average > .7) return "hp-good.png";
+		else if(average > .45)return "hp-low.png";
+		else return "hp-crit.png";
+	}
+	
 	@Override
 	public void resize(int width, int height) {
 	}
@@ -169,5 +203,81 @@ public class BattleScreen implements Screen{
 	public void dispose() {
 	}
 	
+	public void menuattack() {
+ 	stages = new Stage();
+    Gdx.input.setInputProcessor(stages);
+    font = new BitmapFont();
+    skins = new Skin();
+    buttonAtlas = new TextureAtlas(Gdx.files.internal("menu.pack"));
+    skins.addRegions(buttonAtlas);
+    textButtonStyleFight = new TextButtonStyle();
+    textButtonStyleFight.font = font;
+    textButtonStyleFight.up = skins.getDrawable("fight");
+    textButtonStyleFight.down = skins.getDrawable("fight");
+    textButtonStyleFight.checked = skins.getDrawable("fight");
+	textButtonStyleFight.over = skins.getDrawable("fightselec");
+	
+    textButtonStylePokemon = new TextButtonStyle();
+    textButtonStylePokemon.font = font;
+    textButtonStylePokemon.up = skins.getDrawable("poke");
+    textButtonStylePokemon.down = skins.getDrawable("poke");
+    textButtonStylePokemon.checked = skins.getDrawable("poke");
+    textButtonStylePokemon.over = skins.getDrawable("pokeselec");
+    
+    textButtonStyleQuit = new TextButtonStyle();
+    textButtonStyleQuit.font = font;
+    textButtonStyleQuit.up = skins.getDrawable("quit");
+    textButtonStyleQuit.down = skins.getDrawable("quit");
+    textButtonStyleQuit.checked = skins.getDrawable("quit");
+    textButtonStyleQuit.over = skins.getDrawable("quitselec");
+	
+	skin = new Skin();
+	stage = new Stage();
+	batch = new SpriteBatch();
+	img = new Texture("menubackground.png");
+	
+	Gdx.input.setInputProcessor(stage);
+
+	
+
+	// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+	final TextButton fightButton=new TextButton("",textButtonStyleFight);
+	fightButton.setPosition(296, 24);
+	fightButton.setHeight(13);
+	fightButton.setWidth(40);
+	stage.addActor(fightButton);
+	
+	final TextButton pokeButton = new TextButton("",textButtonStylePokemon);
+	pokeButton.setPosition(296,8);
+	pokeButton.setHeight(13);
+	pokeButton.setWidth(51);
+	stage.addActor(pokeButton);
+	
+	final TextButton quitButton = new TextButton("",textButtonStyleQuit);
+	quitButton.setPosition(357,8);
+	quitButton.setHeight(13);
+	quitButton.setWidth(34);
+	stage.addActor(quitButton);
+	
+	fightButton.addListener( new ClickListener() {              
+	    @Override
+	    public void clicked(InputEvent event, float x, float y) {
+	    	//((Game)Gdx.app.getApplicationListener()).setScreen(new BattleScreen());
+	    };
+	});
+	pokeButton.addListener( new ClickListener() {              
+	    @Override
+	    public void clicked(InputEvent event, float x, float y) {
+	    	//Gdx.app.exit();
+	    };
+	});
+	quitButton.addListener( new ClickListener() {              
+	    @Override
+	    public void clicked(InputEvent event, float x, float y) {
+	    	//Gdx.app.exit();
+	    };
+	});
+	
+	}
 
 }
