21b00a0104ae7616e2e1e46fefea6c1882f26331
Joao Costa <sd_joao@hotmail.com>
2015-06-04 18:15:10 +0100

    input

--- core/src/com/pokefight/game/PokeFight.java
+++ core/src/com/pokefight/game/PokeFight.java
@@ -1,6 +1,7 @@
 package com.pokefight.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.pokefight.gamescreens.MenuScreen;
 
 public class PokeFight extends Game {
@@ -8,7 +9,8 @@ public class PokeFight extends Game {
 	@Override
 	public void create() {
 		setScreen(new MenuScreen());
-
+		MenuScreen listener = new MenuScreen();
+		Gdx.input.getTextInput(listener, "Estabelecer ligação", "Valor do IP", null);
 	}
 	
 }
